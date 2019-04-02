decf4867b8ee65afdc6ac2a36549f01720fb9933
mailchenc <mail.chenc@foxmail.com>
2019-04-02 13:30:06 +0800

    南传认证结果文字提示修改 梯形校正适配N20机型报错

--- app/src/main/java/com/jiang/tvlauncher/activity/Launcher_Activity.java
+++ app/src/main/java/com/jiang/tvlauncher/activity/Launcher_Activity.java
@@ -353,7 +353,7 @@ public class Launcher_Activity extends Base_Activity implements View.OnClickList
                                     NanChuan_Ok = false;
                                     LogUtil.e(TAG, "认证失败");
                                     findViewById(R.id.dispaly).setVisibility(View.VISIBLE);
-//                                    Toast.makeText(MyAPP.context, "南方传媒认证失败", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(MyAPP.context, "认证失败", Toast.LENGTH_LONG).show();
                                 } else {
                                     NanChuan_Ok = true;
                                     LogUtil.e(TAG, "认证成功");
--- app/src/main/java/com/jiang/tvlauncher/servlet/Get_Point_Asnc.java
+++ app/src/main/java/com/jiang/tvlauncher/servlet/Get_Point_Asnc.java
@@ -30,47 +30,52 @@ public class Get_Point_Asnc extends AsyncTask<String, Integer, String> {
 
         List<Point.PointBean> beans = new ArrayList<>();
 
-        DlpKeystoneBean bean0 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_LEFT_TOP);
-        LogUtil.e(TAG, "读取左上角>X:" + bean0.getHorizontalValue() + "Y:" + bean0.getVerticalValue());
-        long time0 = new Date().getTime();
-        LogUtil.e(TAG, "结束：" + (time0 - time));
-        Point.PointBean pointBean0 = new Point.PointBean();
-        pointBean0.setIdx(0);
-        pointBean0.setCurrent_x(bean0.getHorizontalValue());
-        pointBean0.setCurrent_x(bean0.getVerticalValue());
-        beans.add(pointBean0);
+        try{
+            DlpKeystoneBean bean0 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_LEFT_TOP);
+            LogUtil.e(TAG, "读取左上角>X:" + bean0.getHorizontalValue() + "Y:" + bean0.getVerticalValue());
+            long time0 = new Date().getTime();
+            LogUtil.e(TAG, "结束：" + (time0 - time));
+            Point.PointBean pointBean0 = new Point.PointBean();
+            pointBean0.setIdx(0);
+            pointBean0.setCurrent_x(bean0.getHorizontalValue());
+            pointBean0.setCurrent_x(bean0.getVerticalValue());
+            beans.add(pointBean0);
 
-        DlpKeystoneBean bean1 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_RIGHT_TOP);
-        LogUtil.e(TAG, "读取右上角>X:" + bean1.getHorizontalValue() + "Y:" + bean1.getVerticalValue());
-        long time1 = new Date().getTime();
-        LogUtil.e(TAG, "结束：" + (time1 - time));
-        Point.PointBean pointBean1 = new Point.PointBean();
-        pointBean1.setIdx(1);
-        pointBean1.setCurrent_x(bean1.getHorizontalValue());
-        pointBean1.setCurrent_x(bean1.getVerticalValue());
-        beans.add(pointBean1);
+            DlpKeystoneBean bean1 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_RIGHT_TOP);
+            LogUtil.e(TAG, "读取右上角>X:" + bean1.getHorizontalValue() + "Y:" + bean1.getVerticalValue());
+            long time1 = new Date().getTime();
+            LogUtil.e(TAG, "结束：" + (time1 - time));
+            Point.PointBean pointBean1 = new Point.PointBean();
+            pointBean1.setIdx(1);
+            pointBean1.setCurrent_x(bean1.getHorizontalValue());
+            pointBean1.setCurrent_x(bean1.getVerticalValue());
+            beans.add(pointBean1);
 
-        DlpKeystoneBean bean2 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_LEFT_BOTTOM);
-        LogUtil.e(TAG, "读取左下角>X:" + bean2.getHorizontalValue() + "Y:" + bean2.getVerticalValue());
-        long time2 = new Date().getTime();
-        LogUtil.e(TAG, "结束：" + (time2 - time));
-        Point.PointBean pointBean2 = new Point.PointBean();
-        pointBean2.setIdx(2);
-        pointBean2.setCurrent_x(bean2.getHorizontalValue());
-        pointBean2.setCurrent_x(bean2.getVerticalValue());
-        beans.add(pointBean2);
+            DlpKeystoneBean bean2 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_LEFT_BOTTOM);
+            LogUtil.e(TAG, "读取左下角>X:" + bean2.getHorizontalValue() + "Y:" + bean2.getVerticalValue());
+            long time2 = new Date().getTime();
+            LogUtil.e(TAG, "结束：" + (time2 - time));
+            Point.PointBean pointBean2 = new Point.PointBean();
+            pointBean2.setIdx(2);
+            pointBean2.setCurrent_x(bean2.getHorizontalValue());
+            pointBean2.setCurrent_x(bean2.getVerticalValue());
+            beans.add(pointBean2);
 
-        DlpKeystoneBean bean3 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_RIGHT_BOTTOM);
-        LogUtil.e(TAG, "读取右下角>X:" + bean3.getHorizontalValue() + "Y:" + bean3.getVerticalValue());
-        long time3 = new Date().getTime();
-        LogUtil.e(TAG, "结束：" + (time3 - time));
-        Point.PointBean pointBean3 = new Point.PointBean();
-        pointBean3.setIdx(3);
-        pointBean3.setCurrent_x(bean3.getHorizontalValue());
-        pointBean3.setCurrent_x(bean3.getVerticalValue());
-        beans.add(pointBean3);
+            DlpKeystoneBean bean3 = DlpKeystoneManager.INSTANCE.getKeystoneValue(DlpKeystoneManager.POINT_RIGHT_BOTTOM);
+            LogUtil.e(TAG, "读取右下角>X:" + bean3.getHorizontalValue() + "Y:" + bean3.getVerticalValue());
+            long time3 = new Date().getTime();
+            LogUtil.e(TAG, "结束：" + (time3 - time));
+            Point.PointBean pointBean3 = new Point.PointBean();
+            pointBean3.setIdx(3);
+            pointBean3.setCurrent_x(bean3.getHorizontalValue());
+            pointBean3.setCurrent_x(bean3.getVerticalValue());
+            beans.add(pointBean3);
 
-        point.setPoint(beans);
+            point.setPoint(beans);
+        }catch(Exception ex){
+            LogUtil.e(TAG, ex.getMessage());
+        }
+
         return null;
     }
 }
